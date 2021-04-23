09a6d14bc44c0fa75e355077bb43d2b1c621124a
walter reyes <walter.reyes@cambridgehn.com>
2021-04-23 13:57:21 -0600

    Segundo ejercicio

--- main.cpp
+++ main.cpp
@@ -21,31 +21,50 @@ int main() {
 
 			for(int i = 0; i<n; i++) {
 				for(int j = 0; j<n; j++) {
-					if(i%2 == 0){
-					
-					if(j%2== 0) {
-						cout<<"0 ";
+					if(i%2 == 0) {
+
+						if(j%2== 0) {
+							cout<<"0 ";
+						} else {
+							cout<<"1 ";
+						}
+
+
 					} else {
-						cout<<"1 ";
+						if(j%2== 0) {
+							cout<<"1 ";
+						} else {
+							cout<<"0 ";
+						}
 					}
-					
-				
-				}else{
-					if(j%2== 0) {
-						cout<<"1 ";
-					} else {
-						cout<<"0 ";
-					}	
-				}
 
 				}
-				
+
 				cout<<endl;
 			}
 			cout<<endl;
-		}
+		}//fin if 1;
 
-	}
+		if(menu == 2) {
+			int n = 0;
+			int aux = 0;
+			cout<<"Ingrese un numero positivo: ";
+			cin>>n;
+			while(n<0) {
+				cout<<"Ingrese un valor para n que sea positivo: ";
+				cin>>n;
+			}
+			for(int i = 0; i <= n; i++) {
+				aux +=2*i*(i-1);
+			}
+			cout<<"El total de la sumatoria es: "<<aux<<endl;
+
+
+		}//fin if 2;
+
+
+	}// fin while menu
+
 
 	return 0;
 }
